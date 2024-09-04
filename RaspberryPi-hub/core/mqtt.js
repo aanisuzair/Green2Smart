b49934edba798a4002c78cc3170a0ad42868b606
Aanis Uzair <68221199+aanisuzair@users.noreply.github.com>
2024-09-04 14:11:18 +0200

    Update mqtt.js

--- RaspberryPi-hub/core/mqtt.js
+++ RaspberryPi-hub/core/mqtt.js
@@ -1,15 +1,5 @@
-/**
- * @company Zebresel - Your Agency for digital Media <hello@zebresel.com> https://www.zebresel.com/
- * @author Kristof Friess
- * @createdAt Fri Mar 01 2024
- * @copyright since 2021 by Zebresel - Your Agency for digital Media. All rights reserved.
- * @version 1.0.0
- * 
- * @description 
- */
-
-import net from 'net'
-import aedes from 'aedes'
+import net from 'net';
+import aedes from 'aedes';
 
 export default class MQTTBroker {
     #opts = {
@@ -25,12 +15,12 @@ export default class MQTTBroker {
     #db     = null;
 
     constructor(opts = {}) {
-        this.#opts = Object.assign(this.#opts, opts);
+        this.#opts = { ...this.#opts, ...opts };
         this.#db = this.#opts.db;
         this.#aedes = aedes();
         this.#server = net.createServer(this.#aedes.handle);
 
-        // authentication
+        // Authentication
         this.#aedes.authenticate = (client, username, password, callback) => {
             if (username && password) {
                 password = Buffer.from(password, 'base64').toString();
@@ -38,35 +28,29 @@ export default class MQTTBroker {
                     return callback(null, true);
                 }
             }
-            const error = new Error('Authentication Failed!! Please enter valid credentials.');
-            console.log('Authentication failed.')
-            return callback(error, false)
-        }
+            const error = new Error('Authentication Failed! Please enter valid credentials.');
+            console.log('Authentication failed.');
+            return callback(error, false);
+        };
 
-        // emitted when a client publishes a message packet on the topic
+        // Handle publish events
         this.#aedes.on('publish', async (packet, client) => {
             if (client) {
-                console.log(`MESSAGE_PUBLISHED : MQTT Client ${(client ? client.id : 'AEDES BROKER_' + aedes.id)} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`);
-
+                console.log(`MESSAGE_PUBLISHED: MQTT Client ${client ? client.id : 'AEDES BROKER_' + aedes.id} has published message "${packet.payload}" on ${packet.topic} to aedes broker ${aedes.id}`);
 
                 let payload = null;
-
-                // Payload should always be json, try to parse it
                 try {
                     payload = JSON.parse(packet.payload);
-                } catch(err) {
-                    console.log(`PARSING_ERROR : MQTT Payload could not be parsed ${packet.payload}`);
+                } catch (err) {
+                    console.log(`PARSING_ERROR: MQTT Payload could not be parsed ${packet.payload}`);
                 }
 
-                if(packet.topic.includes('esp32lr20/state')) {
-                    // Handle relay update over mqtt
+                if (packet.topic.includes('esp32lr20/state')) {
+                    // Update relay state based on incoming payload
                     this.#db.models.Config.handleRelaysUpdate(payload);
-                } else if(packet.topic.includes('esp32hykit/state')) {
-                    // Handle hydroponic kit sensor update over mqtt
-                    this.#db.models.Config.handleHydroponicsKitUpdate(payload);
                 }
             }
-        })
+        });
     }
 
     async start() {
@@ -84,13 +68,13 @@ export default class MQTTBroker {
                 topic: topic,
                 payload: payload
             }, (err) => {
-                if(err) {
+                if (err) {
                     console.log(`MQTT failed to publish to topic ${topic}`);
                     console.log(err);
+                    reject(err);
                 }
                 resolve();
-            })
+            });
         });
     }
-
 }
